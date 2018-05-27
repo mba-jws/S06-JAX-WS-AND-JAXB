d565727f06667068190b3321f38a40c53ef15c31
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2018-05-27 13:15:16 -0700

    refactor of Marshalling and Unmarshalling

--- jaxb-xjc/src/main/java/com/bharath/trainings/jaxb/JAXBDemo.java
+++ jaxb-xjc/src/main/java/com/bharath/trainings/jaxb/JAXBDemo.java
@@ -15,21 +15,26 @@ public class JAXBDemo {
 	public static void main(String[] args) {
 
 		try {
-			JAXBContext context = JAXBContext.newInstance(Patient.class);
-			Marshaller marshaller = context.createMarshaller();
+			// Create Patient object with minimal values
 			Patient patient = new Patient();
 			patient.setId(123);
 			patient.setName("Bharath");
-			
+
+			// Create marshaller anad StringWriter (to hold completed XML)
+			JAXBContext context = JAXBContext.newInstance(Patient.class);
+			Marshaller marshaller = context.createMarshaller();
 			StringWriter writer = new StringWriter();
 			marshaller.marshal(patient, writer);
-			
+
+			// System test, did it work?
 			System.out.println(writer.toString());
-			
+
+			// Create Unmarshaller, and create a new patient object from the previous XML string
 			Unmarshaller unMarshaller = context.createUnmarshaller();
 			Patient patientResult = (Patient) unMarshaller.unmarshal(new StringReader(writer.toString()));
+			
+			// System test, did it work?
 			System.out.print(patientResult.getName());
-					
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
